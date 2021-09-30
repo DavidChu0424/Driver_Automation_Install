9ac539728034e7d668900a44367717b9f7fc8e1b
DavidChu0424 <76864415+DavidChu0424@users.noreply.github.com>
2021-09-30 11:51:06 +0800

    Add files via upload

---
+++ Driver_Installer.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 24 20:30:25 2021
+
+@author: David_Chu
+"""
+import os
+from os import listdir
+from os.path import isfile, isdir, join
+import sys
+
+file_path = str(input("請輸入 Driver 路徑: "))              
+files = os.listdir(file_path)
+files.remove(os.path.basename(__file__))
+
+def execCmd(cmd):
+    r = os.popen(cmd)
+    text = r.read()
+    r.close()
+    return text
+    
+
+def exportTxt(cmdresult, username):
+    with open(username + "_Pass" + ".txt","w") as f:
+      f.write(cmdresult)
+
+
+for i in range(len(files)):
+    cmd = str(files[i]) + "  /s"
+    execCmd(cmd)
+    
+
+    
+          
+
